bbd0b76a20e87d8b9612b4b81e5b44141c90ca07
SoyBean <2648582611@qq.com>
2022-08-14 10:01:32 +0800

    2022/8/14 commit

--- src/views/page4.vue
+++ src/views/page4.vue
@@ -5,48 +5,68 @@
     <div class="con left">
       <!--数据总概-->
       <div class="con_div">
-        <div class="con_div_text left">
-          <div class="con_div_text01 left">
-            <div class="left text01_div">
-              <p>指标1</p>
+        <div class="con_div_text left" style="text-align: center">
 
-              <p>指标1对应数量</p>
+              <p style="font-size: 15px;font-weight:bold;margin-top: 10px">拓扑数据</p>
+              <div style="margin-top: 35px">
+<!--                <div style=" display:inline-block;">交换机数</div>     |-->
+<!--                <div style=" display:inline-block;">主机数</div>     |-->
+<!--                <div style=" display:inline-block;">链路数</div>-->
+                <div class="topo">
+                  <div style="height: 50%;width: 100%">{{ switch_nums }}</div>
+                  <div style="height: 50%;width: 100%">交换机数</div>
+                </div>
+                <div class="topo">
+                  <div style="height: 50%;width: 100%">{{ hosts_nums }}</div>
+                  <div style="height: 50%;width: 100%">主机数</div>
+                </div>
+                <div class="topo">
+                  <div style="height: 50%;width: 100%">{{links_nums}}</div>
+                  <div style="height: 50%;width: 100%">链路数</div>
+                </div>
+              </div>
 
-            </div>
-          </div>
-          <div class="con_div_text01 right">
-            <div class="left text01_div">
-              <p>指标2</p>
-              <p>指标2对应数量</p>
-            </div>
-          </div>
         </div>
-        <div class="con_div_text left">
-          <div class="con_div_text01 left">
-            <div class="left text01_div">
-              <p>指标3</p>
-              <p class="sky">指标3对应数量</p>
-            </div>
-          </div>
-          <div class="con_div_text01 right">
-            <div class="left text01_div">
-              <p>指标4</p>
-              <p class="sky">指标4对应数量</p>
-            </div>
-          </div>
-        </div>
-        <div class="con_div_text left">
+        <div class="con_div_text left" style="text-align: center">
 
-          <div class="con_div_text01 left">
-            <div class="left text01_div">
-              <p>指标5</p>
-              <p class="org">指标5对应数量</p>
+          <p style="font-size: 15px;font-weight:bold;margin-top: 10px">拓扑数据</p>
+          <div style="margin-top: 35px">
+            <!--                <div style=" display:inline-block;">交换机数</div>     |-->
+            <!--                <div style=" display:inline-block;">主机数</div>     |-->
+            <!--                <div style=" display:inline-block;">链路数</div>-->
+            <div class="topo">
+              <div style="height: 50%;width: 100%">{{ switch_nums }}</div>
+              <div style="height: 50%;width: 100%">交换机数</div>
+            </div>
+            <div class="topo">
+              <div style="height: 50%;width: 100%">{{ hosts_nums }}</div>
+              <div style="height: 50%;width: 100%">主机数</div>
+            </div>
+            <div class="topo">
+              <div style="height: 50%;width: 100%">{{links_nums}}</div>
+              <div style="height: 50%;width: 100%">链路数</div>
             </div>
           </div>
-          <div class="con_div_text01 right">
-            <div class="left text01_div">
-              <p>指标6</p>
-              <p class="org">指标6对应数量</p>
+
+        </div>
+        <div class="con_div_text left" style="text-align: center">
+
+          <p style="font-size: 15px;font-weight:bold;margin-top: 10px">拓扑数据</p>
+          <div style="margin-top: 35px">
+            <!--                <div style=" display:inline-block;">交换机数</div>     |-->
+            <!--                <div style=" display:inline-block;">主机数</div>     |-->
+            <!--                <div style=" display:inline-block;">链路数</div>-->
+            <div class="topo">
+              <div style="height: 50%;width: 100%">{{ switch_nums }}</div>
+              <div style="height: 50%;width: 100%">交换机数</div>
+            </div>
+            <div class="topo">
+              <div style="height: 50%;width: 100%">{{ hosts_nums }}</div>
+              <div style="height: 50%;width: 100%">主机数</div>
+            </div>
+            <div class="topo">
+              <div style="height: 50%;width: 100%">{{links_nums}}</div>
+              <div style="height: 50%;width: 100%">链路数</div>
             </div>
           </div>
         </div>
@@ -55,65 +75,33 @@
       <div class="div_any">
         <div class="left div_any01">
           <div class="div_any_child">
-            <div class="div_any_title">图表1</div>
-<!--            <span class="angle1"></span>-->
-<!--            <span class="angle2"></span>-->
-<!--            <span class="angle3"></span>-->
-<!--            <span class="angle4"></span>-->
+            <div class="div_any_title">flow统计</div>
+            <bar-chart :config="configData4"></bar-chart>
 
           </div>
           <div class="div_any_child">
             <div class="div_any_title">图表2</div>
-
+            <bar-chart :config="configData4"></bar-chart>
           </div>
         </div>
         <div class="div_any02 left ">
           <div class="div_any_child div_height">
             <div class="div_any_title any_title_width">拓扑发现</div>
-            <iframe :src='address' style="height: 100%;width:100%;border-radius:20px;border: none"></iframe>
+            <iframe :src='address' style="height: 100%;width:100%;border-radius:20px;border: none" frameborder="0"></iframe>
           </div>
         </div>
         <div class="right div_any01">
           <div class="div_any_child">
             <div class="div_any_title">图表4</div>
+            <bar-chart :config="configData4"></bar-chart>
           </div>
           <div class="div_any_child">
             <div class="div_any_title">图表5</div>
+            <bar-chart :config="configData4"></bar-chart>
           </div>
         </div>
       </div>
 
-<!--      <div id="el-dialog" class="">-->
-<!--        <div class="xc_layer"></div>-->
-<!--        <div class="popBox" id="printView">-->
-<!--          <div class="ttBox"><span class="tt" id="reportTitle">第一医院</span><img src="../images/close.png" style="width: 30px;float: right;cursor: pointer;" title="关闭弹窗" class="close"/></div>-->
-<!--          <div class="txtBox" id="el-dialog_body">-->
-<!--            <div style="height:100%;width: 98%;margin-left: 1%;">-->
-<!--              <div class="left div_any01" style="width: 64%;">-->
-<!--                <div class="div_any_child">-->
-<!--                  <div class="div_any_title"><div type="text" class="demo-input" id="date1" style="display: inline-block;cursor: pointer;margin-right: 16px;"></div><img src="../images/title_4.png">门诊住院人次</div>-->
-<!--                  <p id="lineChart3" class="p_chart"></p>-->
-<!--                </div>-->
-<!--                <div class="div_any_child">-->
-<!--                  <div class="div_any_title"><div type="text" class="demo-input" id="date2" style="display: inline-block;cursor: pointer;margin-right: 16px;"></div><img src="../images/title_7.png">医疗费用</div>-->
-<!--                  <p id="lineChart4" class="p_chart"></p>-->
-<!--                </div>-->
-<!--              </div>-->
-<!--              <div class="left div_any01"  style="width: 32%;">-->
-<!--                <div class="div_any_child">-->
-<!--                  <div class="div_any_title"><div type="text" class="demo-input" id="date3" style="display: inline-block;cursor: pointer;margin-right: 16px;"></div><img src="../images/title_18.png">病人年龄段分布</div>-->
-<!--                  <p id="pieChart2" class="p_chart"></p>-->
-<!--                </div>-->
-<!--                <div class="div_any_child">-->
-<!--                  <div class="div_any_title"><div type="text" class="demo-input" id="date4" style="display: inline-block;cursor: pointer;margin-right: 16px;"></div><img src="../images/title_20.png">医疗费用组成</div>-->
-<!--                  <p id="pieChart3" class="p_chart"></p>-->
-<!--                </div>-->
-<!--              </div>-->
-
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
 
     </div>
 
@@ -125,14 +113,28 @@
 
 <script>
 
+import axios from "axios";
+axios.defaults.baseURL = '/api';
+//引入柱形图
+const barChart = () => import('./components/page4/barChart');
 export default {
   name: 'page4',
   props: ['selectRangeDate'],
   components: {
-
+    barChart
   },
   data() {
     return {
+      //柱形图假数据
+      configData4: {
+        data: [213, 190, 137, 99, 63, 196, 248, 212, 248, 112]
+      },
+      //交换机数量
+      switch_nums:0,
+      //主机数量
+      hosts_nums:0,
+      //链路数量
+      links_nums:0,
       address:'',
       everyPer: 0,
       xOffset: 0,
@@ -157,15 +159,55 @@ export default {
     }
   },
   methods: {
-
+    //获取交换机数量
+    getSwitchNums(){
+      const vm = this;
+      axios.get('/stats/switches')
+          .then(function (response) {
+            console.log(response.data.length);
+            vm.switch_nums=response.data.length
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    },
+    //获取主机数量v1.0/topology/hosts
+    getHostsNums(){
+      const vm = this;
+      axios.get('/v1.0/topology/hosts')
+          .then(function (response) {
+            console.log(response.data.length);
+            vm.hosts_nums=response.data.length
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    },
+    //获取交换机之间链路数量
+    getLinksNums(){
+      const vm = this;
+      axios.get('/v1.0/topology/links')
+          .then(function (response) {
+            console.log(response.data.length);
+            vm.links_nums=response.data.length
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
   },
 
   mounted() {
-    // this.getAddress();
+    //获取虚拟机的ip地址与端口
     this.ip=this.$route.query.ip;
     this.port=this.$route.query.port;
     this.address='http://'+this.$route.query.ip+':'+this.$route.query.port;
-    console.log(this.address)
+    console.log(this.address);
+    this.getSwitchNums();
+    this.getHostsNums();
+    this.getLinksNums();
+
+
   },
   beforeDestroy(){
 
@@ -222,5 +264,15 @@ export default {
   border-bottom: 2px solid grey;
   border-right: 2px solid grey;
 
+}
+.topo{
+  display:inline-block;
+  float: left;
+  margin-left: 35px;
+  margin-top: -18px;
+  width: 120px;
+  height: 100px;
+  /*background-color: yellow;*/
+  font-weight: bold;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,20 @@ module.exports = {
                 javascriptEnabled: true
             }
         }
+    },
+    devServer: {
+        host: '127.0.0.1',
+        port: 8081,
+        open: true,// vue项目启动时自动打开浏览器
+        proxy: {
+            '/api': { // '/api'是代理标识，用于告诉node，url前面是/api的就是使用代理的
+                target: "http://192.168.190.129:8080", //目标地址，一般是指后台服务器地址
+                changeOrigin: true, //是否跨域
+                pathRewrite: { // pathRewrite 的作用是把实际Request Url中的'/api'用""代替
+                    '^/api': ""
+                }
+            }
+        }
     }
 }
+
